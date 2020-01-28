33ee263c0bf767f8a095caf4f22621a5b9a9ccc1
David Bogatin <davidbo@bu.edu>
2020-01-28 16:15:36 -0500

    made a better gym

--- 01-python/leisure/gym.py
+++ 01-python/leisure/gym.py
@@ -1,8 +1,12 @@
-def draw_gym():
-    print("GYM")
-    print("0-0")
-    print(" 0 ")
-    print("'/|\"")
-    print("'/ \"")
-
+def draw_gym(length=5, width=5):
+    for i in range(length):
+        print("GYM")
+        print(" 0 ")
+        print("'/|\"")
+        print("/ \"")
+    for i in range(width):
+        print("0-0")
+        print(" 0 ")
+        print("'/|\"")
+        print("/ \"")
     return
